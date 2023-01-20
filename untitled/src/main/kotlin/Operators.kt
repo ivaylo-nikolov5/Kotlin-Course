501691cdb01f9dd545ecfa094dd8c319897f8de5
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-20 15:21:16 +0200

    Operations part

---
+++ untitled/src/main/kotlin/Operators.kt
@@ -0,0 +1,37 @@
+fun main() {
+    var x = 5
+    val y = 3
+
+    println("x + y = ${x + y}")
+    println("x - y = ${x - y}")
+    println("x * y = ${x * y}")
+    println("x / y = ${x / y}")
+    println("x % y = ${x % y}")
+
+    var result = x + y
+
+    result += 2
+    println("result = $result")
+
+    result -= 2
+    println("result = $result")
+
+    result *= 2
+    println("result = $result")
+
+    result /= 2
+    println("result = $result")
+
+    result %= 2
+    println("result = $result")
+
+    println("3 + 3 * 4 = ${(3 + 3) * 4}")
+    x = 0
+
+    println("x = ${x++}")
+    println("x = ${++x}")
+
+    println("x = ${x--}")
+    println("x = ${--x}")
+
+}
