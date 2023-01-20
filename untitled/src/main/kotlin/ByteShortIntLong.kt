777fb379ab88b8e63051fb9f23eeb6a63c459259
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-20 13:47:14 +0200

    Byte, short, int, long part

---
+++ untitled/src/main/kotlin/ByteShortIntLong.kt
@@ -0,0 +1,31 @@
+fun main() {
+    var number: Int = 17
+
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    val minIntegerValue: Int = Int.MIN_VALUE
+
+    println("Int maximum value is $maxIntegerValue")
+    println("Int minimum value is $minIntegerValue")
+
+    number = 2147483647
+
+    val myMaxByteValue: Byte = Byte.MAX_VALUE
+    val myMinByteValue: Byte = Byte.MIN_VALUE
+
+    println("Byte maximum value is $myMaxByteValue")
+    println("Byte minimum value is $myMinByteValue")
+
+    val myMaxShortValue: Short = Short.MAX_VALUE
+    val myMinShortValue: Short = Short.MIN_VALUE
+
+    println("Short maximum value is $myMaxShortValue")
+    println("Short minimum value is $myMinShortValue")
+
+    val myMaxLongValue: Long = Long.MAX_VALUE
+    val myMinLongValue: Long = Long.MIN_VALUE
+
+    println("Long maximum value is $myMaxLongValue")
+    println("Long minimum value is $myMinLongValue")
+
+    val myNumber = 2888888888L
+}
