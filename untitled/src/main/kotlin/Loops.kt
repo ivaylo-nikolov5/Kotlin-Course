11bcf64c50dd03fffb6e851113028b31a91ec3bf
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-23 14:48:08 +0200

    For loop part

---
+++ untitled/src/main/kotlin/Loops.kt
@@ -0,0 +1,27 @@
+fun main() {
+//    for loop
+
+//    for (i in 1..10) {
+//        println("i = $i")
+//    }
+//
+//    for (i in 1 until 10) {
+//        println(i)
+//    }
+//
+//    for (i in 10 downTo  1) {
+//        println(i)
+//    }
+//
+//    for (i in 1 until 10 step 2) {
+//        println(i)
+//    }
+
+//    for (i in 1..10) {
+//        println(i)
+//    }
+
+//    while loop
+
+
+}
