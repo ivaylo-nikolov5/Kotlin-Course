7561c3f0d49cf64f9224a4ff820fe4794ed4d669
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-01-23 15:16:43 +0200

    While loop and nested loops part

--- untitled/src/main/kotlin/Loops.kt
+++ untitled/src/main/kotlin/Loops.kt
@@ -24,4 +24,39 @@ fun main() {
 //    while loop
 
 
+//    while (number < 10) {
+//        number++
+//        if (number == 7) {
+//            break
+//        }
+//        println(number)
+//    }
+
+//    for (i in 1..10) {
+//        if (i == 7) {
+//            break
+//        }
+//        println(i)
+//    }
+
+//    do {
+//        println(number)
+//    } while (number < 10)
+
+    var number = 0
+
+    outer@ while (number < 5) {
+        number++
+        println(number)
+
+        var i = 0
+
+        while (i < 5) {
+            if (i == 0) {
+                break@outer
+            }
+            i++
+            println("***$i")
+        }
+    }
 }
